ff221573f280e699fabb66c831c2b10eb2709750
Angel Cruz <angi_ed01@hotmail.com>
2020-08-30 10:26:37 -0500

    Docs added

---
+++ docs/precache-manifest.9ffa85557f9c64af82f71026aa6c192a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7887eb9d1423c18984a0c4894d8e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a50a4ee87a0cc26f590",
+    "url": "/static/css/main.c2a5e8c7.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a50a4ee87a0cc26f590",
+    "url": "/static/js/main.f98cc87a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
